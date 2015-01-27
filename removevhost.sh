f386889c6780d065f2ec0b463f4a92beaa762fc6
Christopher Reeves <chris@reeves.net.nz>
2015-01-27 15:52:33 +0930

    Added completion notification

--- removevhost.sh
+++ removevhost.sh
@@ -6,3 +6,4 @@ service apache2 restart
 rm -rf /var/www/vhosts/$domain
 rm -rf /var/log/apache2/$domain*.*
 rm -rf /etc/apache2/sites-available/$domain
+echo "Removed $domain, log files and conf file
