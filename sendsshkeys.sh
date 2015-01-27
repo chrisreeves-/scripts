d9670a8b7b07cd92eba6e8fee5d5d834a198d3ac
Christopher Reeves <chris@reeves.net.nz>
2015-01-27 15:59:40 +0930

    Added completion notification

--- sendsshkeys.sh
+++ sendsshkeys.sh
@@ -2,6 +2,5 @@
 hostname=$HOSTNAME
 echo -e "What server are you sending the key to?: \c "
 read server
-echo -e "What user name do you want to pass credentials as?: \c "
-read user
 scp /root/.ssh/id_rsa root@$server:/root/.ssh/id_rsa.$hostname
+echo "Sent root ssh key to $server"
