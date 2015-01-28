26bef141998cea74771b82ef50424c802f094155
Christopher Reeves <chris@reeves.net.nz>
2015-01-28 11:48:34 +0930

    Added specification of vhost location

--- createvhost.sh
+++ createvhost.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo -e "Please specify domain name: \c "
 read domain
-mkdir -p /var/www/vhosts/$domain
-chown www-data:www-data /var/www/vhosts/$domain -R
+echo -e "Please specify the root vhost location *WITHOUT* trailing / example: /var/www/vhosts"
+read vhost
+mkdir -p $vhost/$domain
+chown www-data:www-data $vhost/$domain -R
 touch /var/log/apache2/$domain-error.log
 touch /var/log/apache2/$domain-access.log
 touch /etc/apache2/sites-available/$domain
 echo -e "<VirtualHost *:80>" >> /etc/apache2/sites-available/$domain
 echo -e "       ServerName $domain" >> /etc/apache2/sites-available/$domain
 echo -e "       ServerAlias www.$domain" >> /etc/apache2/sites-available/$domain
-echo -e "       DocumentRoot /var/www/vhosts/$domain" >> /etc/apache2/sites-available/$domain
-echo -e "               <Directory /var/www/vhosts/$domain>" >> /etc/apache2/sites-available/$domain
+echo -e "       DocumentRoot $vhost/$domain" >> /etc/apache2/sites-available/$domain
+echo -e "               <Directory $vhost/$domain>" >> /etc/apache2/sites-available/$domain
 echo -e "                       Options Indexes FollowSymLinks MultiViews" >> /etc/apache2/sites-available/$domain
 echo -e "                       AllowOverride all" >> /etc/apache2/sites-available/$domain
 echo -e "               </Directory>" >> /etc/apache2/sites-available/$domain
@@ -19,12 +21,12 @@ echo -e "       CustomLog /var/log/apache2/$domain-access.log combined" >> /etc/
 echo -e "</VirtualHost>" >> /etc/apache2/sites-available/$domain
 a2ensite $domain
 /etc/init.d/apache2 restart
-touch /var/www/vhosts/$domain/index.html
-echo -e "<!DOCTYPE html>" >> /var/www/vhosts/$domain/index.html
-echo -e "<html>" >> /var/www/vhosts/$domain/index.html
-echo -e "<body>" >> /var/www/vhosts/$domain/index.html
-echo -e "<h1> Welcome to $domain</h1>" >> /var/www/vhosts/$domain/index.html
-echo -e "<p>This is the test page of $domain</p>" >> /var/www/vhosts/$domain/index.html
-echo -e "</body>" >> /var/www/vhosts/$domain/index.html
-echo -e "</html>" >> /var/www/vhosts/$domain/index.html
-echo "Created site $domain located at /var/www/vhosts/$domain"
+touch $vhost/$domain/index.html
+echo -e "<!DOCTYPE html>" >> $vhost/$domain/index.html
+echo -e "<html>" >> $vhost/$domain/index.html
+echo -e "<body>" >> $vhost/$domain/index.html
+echo -e "<h1> Welcome to $domain</h1>" >> $vhost/$domain/index.html
+echo -e "<p>This is the test page of $domain</p>" >> $vhost/$domain/index.html
+echo -e "</body>" >> $vhost/$domain/index.html
+echo -e "</html>" >> $vhost/$domain/index.html
+echo "Created site $domain located at $vhost/$domain"
