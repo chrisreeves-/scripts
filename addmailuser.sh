fd26da10e57253971f15520f0724ce35a643a197
Christopher Reeves <chris@reeves.net.nz>
2015-02-01 14:44:03 +0930

    Added error handling no user input

--- addmailuser.sh
+++ addmailuser.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo -e "Please specify mail username: \c "
 read user
+# Check no response
+if [ -z $user];
+  then
+    echo "You did not enter a user name"
+    exit
+fi
 adduser $user
 echo -e "$user: $user" >> /etc/aliases
 echo Activating new alias
