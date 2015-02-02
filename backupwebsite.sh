b497b3476329a15542b364ba7e9b1c8507d41b79
Christopher Reeves <chris@reeves.net.nz>
2015-02-02 14:18:21 +0930

    Added vhost location query, commented and added no user input handling

--- backupwebsite.sh
+++ backupwebsite.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 date=(`date +%d-%m-%Y-%I-%M`)
-# echo -e "What is the root to the websites: \c "
-# read var
+echo -e "Please specify the root vhost location *WITHOUT* trailing / example: /var/www/vhosts"
+read vhost
 echo -e "What is the domain name: \c "
 read domain
-echo ## Create backup location ##
+# Check no response for vhost location
+if [ -z $vhost];
+  then
+    echo "You did not enter a vhost location"
+    exit
+fi
+# Check no response for domain name
+if [ -z $domain];
+  then
+    echo "You did not enter a domain name"
+    exit
+fi
+## Create backup location ##
 mkdir -p /backups/$domain
-tar cvf /backups/$domain/$domain-$date.tar /var/www/vhosts/$domain
+# Compressing and copy to backup location
+tar cvf /backups/$domain/$domain-$date.tar $vhost/$domain
+# Advising of backup location to user
 echo "Backup is stored in /backups/$domain"
