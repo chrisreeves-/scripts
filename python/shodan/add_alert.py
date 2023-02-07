5d6390bc7791670232da71d6c3e5f122b3f8b56b
Christopher Reeves <chris@reeves.net.nz>
2023-02-07 10:13:03 +1000

    Making Shodan more verbose and adding handling

--- python/shodan/add_alert.py
+++ python/shodan/add_alert.py
@@ -1,3 +1,5 @@
+import click
+import shodan
 from shodan import Shodan
 
 shodan_api = "YOUR-SHODAN-API-KEY"
@@ -5,21 +7,41 @@ name = "Alert Name Here"
 cidr_vm = "IP-TO-MONITOR"
 
 # Connect
-api = Shodan(shodan_api)
+try:
+    api = Shodan(shodan_api)
+    print(f"HTTP 200 - Connected to Shodan API")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
 
 # Create Alert
-api.create_alert(name=name, ip=cidr_vm, expires=0)
+try:
+    api.create_alert(name=name, ip=cidr_vm, expires=0)
+    print(f"HTTP 200 - Created alert {name}")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
 
 # Get Alert ID
-data = api.alerts(include_expired=True)
-for i in data:
-    alert = i['name']
-    if alert == name:
-        alert_id = i['id']
+try:
+    data = api.alerts(include_expired=True)
+    for i in data:
+        alert = i['name']
+        if alert == name:
+            alert_id = i['id']
+    print(f"HTTP 200 - Retrieved alert ID={alert_id}")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
 
 # Add Alert Triggers
-api.enable_alert_trigger(aid=alert_id,
-                         trigger='industrial_control_system,internet_scanner,iot,malware,new_service,open_database,ssl_expired,vulnerable')
+try:
+    api.enable_alert_trigger(aid=alert_id,
+                             trigger='industrial_control_system,internet_scanner,iot,malware,new_service,open_database,ssl_expired,vulnerable')
+    print(f"HTTP 200 - Added triggers to ID={alert_id}")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
+
 
 # Add Alert Notifications
-api.add_alert_notifier(aid=alert_id, nid='default')
+try:
+    api.add_alert_notifier(aid=alert_id, nid='default')
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
--- python/shodan/delete_alert.py
+++ python/shodan/delete_alert.py
@@ -1,17 +1,31 @@
+import click
+import shodan
 from shodan import Shodan
 
 shodan_api = "YOUR-SHODAN-API-KEY"
 name = "Alert Name Here"
 
 # Connect
-api = Shodan(shodan_api)
+try:
+    api = Shodan(shodan_api)
+    print(f"HTTP 200 - Connected to Shodan API")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
 
 # Get Alert ID
-data = api.alerts(include_expired=True)
-for i in data:
-    alert = i['name']
-    if alert == name:
-        alert_id = i['id']
+try:
+    data = api.alerts(include_expired=True)
+    for i in data:
+        alert = i['name']
+        if alert == name:
+            alert_id = i['id']
+    print(f"HTTP 200 - Retrieved alert ID={alert_id}")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
 
 # Delete Alert
-api.delete_alert(aid=alert_id)
+try:
+    api.delete_alert(aid=alert_id)
+    print(f"HTTP 200 - Deleted alert ID={alert_id}")
+except shodan.APIError as e:
+    raise click.ClickException(e.value)
