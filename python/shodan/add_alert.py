a21a0eefbd0dd9dcd20475297ae01602f377c2b8
Christopher Reeves <chris@reeves.net.nz>
2023-03-09 12:26:10 +1000

    Updating to be more robust

--- python/shodan/add_alert.py
+++ python/shodan/add_alert.py
@@ -1,47 +1,66 @@
+import sys
 import click
 import shodan
 from shodan import Shodan
 
-shodan_api = "YOUR-SHODAN-API-KEY"
-name = "Alert Name Here"
-cidr_vm = "IP-TO-MONITOR"
+'''
+Date:        8th March 2023
+Author:      Chris Reeves
+Description: Checks to see if an existing Shodan asset to monitor
+             has been created and creates if none is present then
+             checks to make sure it was created if not already.
+'''
 
-# Connect
+# Variables
+shodan_api = ""
+shodan_name = ""
+shodan_cidr = ""
+triggers = 'industrial_control_system,' \
+           'internet_scanner,' \
+           'iot,' \
+           'malware,' \
+           'new_service,' \
+           'open_database,' \
+           'ssl_expired,' \
+           'vulnerable'
+
+# Connect to Shodan
 try:
     api = Shodan(shodan_api)
     print(f"HTTP 200 - Connected to Shodan API")
 except shodan.APIError as e:
     raise click.ClickException(e.value)
 
-# Create Alert
-try:
-    api.create_alert(name=name, ip=cidr_vm, expires=0)
-    print(f"HTTP 200 - Created alert {name}")
-except shodan.APIError as e:
-    raise click.ClickException(e.value)
+# Checking for existing alert monitor and exit if already exists
+data = api.alerts(include_expired=False)
+for i in data:
+    alert = i['name']
+    if alert == shodan_name:
+        print("HTTP 500 - Existing asset found ...")
+        sys.exit(1)
 
-# Get Alert ID
-try:
-    data = api.alerts(include_expired=True)
-    for i in data:
-        alert = i['name']
-        if alert == name:
-            alert_id = i['id']
-    print(f"HTTP 200 - Retrieved alert ID={alert_id}")
-except shodan.APIError as e:
-    raise click.ClickException(e.value)
+# Creating alert monitor
+api.create_alert(name=shodan_name,
+                 ip=shodan_cidr,
+                 expires=0)
+print(f"HTTP 200 - Created asset {shodan_name} ...")
+print("Getting existing asset ...")
+results = api.alerts(include_expired=False)
+for i in results:
+    alert = i['name']
+    if alert == shodan_name:
+        alert_id = i['id']
+        print(f"HTTP 200 - Retrieved asset ID={alert_id}")
+        api.enable_alert_trigger(aid=alert_id,
+                                 trigger=triggers)
+        print(f"HTTP 200 - Added triggers to asset ID={alert_id}")
+        api.add_alert_notifier(aid=alert_id,
+                               nid='default')
 
-# Add Alert Triggers
-try:
-    api.enable_alert_trigger(aid=alert_id,
-                             trigger='industrial_control_system,internet_scanner,iot,malware,new_service,open_database,ssl_expired,vulnerable')
-    print(f"HTTP 200 - Added triggers to ID={alert_id}")
-except shodan.APIError as e:
-    raise click.ClickException(e.value)
-
-
-# Add Alert Notifications
-try:
-    api.add_alert_notifier(aid=alert_id, nid='default')
-except shodan.APIError as e:
-    raise click.ClickException(e.value)
+# Check to if the monitor is created
+for i in results:
+    alert = i['name']
+    if alert == shodan_name:
+        print("Asset has been created successfully and has been tested ...")
+    elif alert == "":
+        print("Could not find the new asset ...")
