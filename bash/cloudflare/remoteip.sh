14232012d62bed640a2c5341295f326bc806d043
chris <chris@reeves.net.nz>
2023-05-25 16:32:32 +1000

    Adding ipv6 support

--- bash/cloudflare/remoteip.sh
+++ bash/cloudflare/remoteip.sh
@@ -1,18 +1,22 @@
 # https://developers.cloudflare.com/support/troubleshooting/restoring-visitor-ips/restoring-original-visitor-ips/
 
 output_file="/etc/apache2/conf-available/remoteip.conf"
-ip_addresses=$(curl https://www.cloudflare.com/ips-v4)
+ip_addresses_v4=$(curl https://www.cloudflare.com/ips-v4)
+ip_addresses_v6=$(curl https://www.cloudflare.com/ips-v6)
 
 if [ ! -f /etc/apache2/conf-available/remoteip.conf ]
 then
     echo "Cloudflare remoteip config not found, creating ..."
     echo "RemoteIPHeader CF-Connecting-IP" > "$output_file"
-    for ip in $ip_addresses; do
+    for ip in $ip_addresses_v4; do
+      echo "RemoteIPTrustedProxy $ip" >> "$output_file"
+    done
+    for ip in $ip_addresses_v6; do
       echo "RemoteIPTrustedProxy $ip" >> "$output_file"
     done
 else
     echo "Cloudflare remoteip config found, skipping ..."
 fi
 
-sed -i 's/LogFormat \"%h %l %u %t \\\"%r\\\" %>s %O \\\"%{Referer}i\\" \\"%{User-Agent}i\\i\\"" combined/LogFormat "%a %l %u %t \\"%r\\" %>s %O \\"%{Referer}i\\" \\"%{User-Agent}i\\"" combined/g' /etc/apache2/apache2.conf
-a2enconf remoteip
+sed -i 's/LogFormat \"%h %l %u %t \\\"%r\\\" %>s %O \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" combined/LogFormat "%a %l %u %t \\"%r\\" %>s %O \\"%{Referer}i\\" \\"%{User-Agent}i\\"" combined/g' /etc/apache2/apache2.conf
+
