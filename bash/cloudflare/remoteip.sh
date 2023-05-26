dcaa4635f31a4750629df18c9ba4f521511ca890
chris <chris@reeves.net.nz>
2023-05-26 10:41:08 +1000

    Fleshing out the detail of what steps do what

--- bash/cloudflare/remoteip.sh
+++ bash/cloudflare/remoteip.sh
@@ -4,6 +4,7 @@ output_file="/etc/apache2/conf-available/remoteip.conf"
 ip_addresses_v4=$(curl https://www.cloudflare.com/ips-v4)
 ip_addresses_v6=$(curl https://www.cloudflare.com/ips-v6)
 
+# Add Cloudflare IPs to the remoteip config
 if [ ! -f /etc/apache2/conf-available/remoteip.conf ]
 then
     echo "Cloudflare remoteip config not found, creating ..."
@@ -18,5 +19,17 @@ else
     echo "Cloudflare remoteip config found, skipping ..."
 fi
 
+# Enable the Apache mod remoteip module
+a2enmod remoteip
+
+# Update the log format
 sed -i 's/LogFormat \"%h %l %u %t \\\"%r\\\" %>s %O \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" combined/LogFormat "%a %l %u %t \\"%r\\" %>s %O \\"%{Referer}i\\" \\"%{User-Agent}i\\"" combined/g' /etc/apache2/apache2.conf
 
+# Enable the remoteip config
+a2enconf remoteip
+
+# Test the Apache config
+apache2ctl configtest
+
+# If the config test is successful, restart Apache
+systemctl restart apache2
